42e8a5902535825f9c49939a57fa8e6dcc9060a8
周琼 <justin.qiong@outllok.com>
2018-04-13 08:33:53 +0800

    调整ImageTypeEnum位置

---
+++ src/main/java/com/wan/sys/entity/image/ImageTypeEnum.java
@@ -0,0 +1,28 @@
+package com.wan.sys.entity.image;
+
+/**
+ * 图片类型枚举
+ */
+public enum ImageTypeEnum {
+
+    MESSAGE("1"),
+    PHOTO("2"),
+    DYNAMIC("3"),
+    AVATAR("4"),
+    LOST("5"),
+    FORUM("6");
+
+    private String index;
+
+    ImageTypeEnum(String index) {
+        this.index = index;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+}
