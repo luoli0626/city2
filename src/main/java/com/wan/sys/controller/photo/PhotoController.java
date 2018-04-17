b9dadcf89f72575f8d3220ee121b5c1ea824bf6a
周琼 <justin.qiong@gmail.com>
2018-04-17 16:07:03 +0800

    随手拍接口不返回用户审核信息

--- src/main/java/com/wan/sys/controller/photo/PhotoController.java
+++ src/main/java/com/wan/sys/controller/photo/PhotoController.java
@@ -20,6 +20,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
 
+import java.util.List;
+
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
@@ -84,6 +86,13 @@ public class PhotoController {
         if (result.hasErrors()) {
             return ValidUtil.errorResponse(result);
         }
-        return new ResponseSuccess(photoService.getList(query));
+        List<Photo> photos = photoService.getList(query);
+        if (photos != null) {
+            for (Photo photo : photos) {
+                photo.setCreateUserName(null);
+                photo.setAllState(null);
+            }
+        }
+        return new ResponseSuccess(photos);
     }
 }
